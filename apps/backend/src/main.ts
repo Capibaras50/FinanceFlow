448d49e22b0fca39dd05626c4f33ca2d1837f4b9
Capibaras50 <danielfelipesaenzbaquero@gmail.com>
2026-06-30 13:05:26 -0500

    feat: Add ClassSerializerInterceptor globally

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
